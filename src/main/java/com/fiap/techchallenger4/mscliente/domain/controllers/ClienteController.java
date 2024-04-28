50b1e2c9e694fcfa74b69b51a96947c406f20f21
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2024-04-28 05:12:32 -0300

    Inserido novos endpoints: Busca, Atualiza e Apaga cliente pelo CPF, mais as validações dos dados.

--- src/main/java/com/fiap/techchallenger4/mscliente/domain/controllers/ClienteController.java
+++ src/main/java/com/fiap/techchallenger4/mscliente/domain/controllers/ClienteController.java
@@ -36,6 +36,14 @@ public class ClienteController {
         return SpringControllerUtils.response(HttpStatus.OK, () -> clienteService.buscarClientePorCodigo(codigoCliente));
     }
 
+    @GetMapping("cpf/{cpf}")
+    @Operation(summary = "Buscar cliente por CPF")
+    @ApiResponseSwaggerOk
+    @ApiResponseSwaggerNoContent
+    public ResponseEntity<?> buscarClientePorCpf(@PathVariable String cpf) {
+        return SpringControllerUtils.response(HttpStatus.OK, () -> clienteService.buscarClientePorCpf(cpf));
+    }
+
     @PostMapping
     @Operation(summary = "Cadastrar um novo cliente")
     @ApiResponseSwaggerCreate
@@ -44,18 +52,35 @@ public class ClienteController {
     }
 
     @PutMapping("/{codigoCliente}")
-    @Operation(summary = "Atualizar um cliente")
+    @Operation(summary = "Atualizar um cliente pelo Código de Cliente")
     @ApiResponseSwaggerOk
-    public ResponseEntity<?> atualizarCliente(@PathVariable Long codigoCliente, @RequestBody ClienteDtoRequest clienteDto) {
-        return SpringControllerUtils.response(HttpStatus.OK, () -> clienteService.atualizarCliente(codigoCliente, clienteDto));
+    public ResponseEntity<?> atualizarClientePorCodigo(@PathVariable Long codigoCliente, @RequestBody ClienteDtoRequest clienteDto) {
+        return SpringControllerUtils.response(HttpStatus.OK, () -> clienteService.atualizarClientePorCodigo(codigoCliente, clienteDto));
+    }
+
+    @PutMapping("cpf/{cpf}")
+    @Operation(summary = "Atualizar um cliente pelo CPF")
+    @ApiResponseSwaggerOk
+    public ResponseEntity<?> atualizarClientePorCpf(@PathVariable String cpf, @RequestBody ClienteDtoRequest clienteDto) {
+        return SpringControllerUtils.response(HttpStatus.OK, () -> clienteService.atualizarClientePorCpf(cpf, clienteDto));
     }
 
     @DeleteMapping("/{codigoCliente}")
-    @Operation(summary = "Excluir um cliente")
+    @Operation(summary = "Excluir um cliente pelo Código de Cliente")
     @ApiResponseSwaggerNoContent
-    public ResponseEntity<?> excluirCliente(@PathVariable Long codigoCliente) {
+    public ResponseEntity<?> excluirClientePorCodigo(@PathVariable Long codigoCliente) {
         return SpringControllerUtils.response(HttpStatus.NO_CONTENT, () -> {
-            clienteService.excluirCliente(codigoCliente);
+            clienteService.excluirClientePorCodigo(codigoCliente);
+            return null;
+        });
+    }
+
+    @DeleteMapping("cpf/{cpf}")
+    @Operation(summary = "Excluir um cliente pelo CPF")
+    @ApiResponseSwaggerNoContent
+    public ResponseEntity<?> excluirClientePorCpf(@PathVariable String cpf) {
+        return SpringControllerUtils.response(HttpStatus.NO_CONTENT, () -> {
+            clienteService.excluirClientePorCpf(cpf);
             return null;
         });
     }
--- src/main/java/com/fiap/techchallenger4/mscliente/domain/services/ClienteService.java
+++ src/main/java/com/fiap/techchallenger4/mscliente/domain/services/ClienteService.java
@@ -16,51 +16,51 @@ public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    private ClienteEntity buscarClienteEntity(Long codigoCliente) throws BusinessException {
+    private ClienteEntity findByCodigoCliente(Long codigoCliente) throws BusinessException {
         ClienteEntity cliente = clienteRepository.findByCodigoCliente(codigoCliente);
         if (cliente == null) {
-            throw new BusinessException("Cliente não encontrado");
+            throw new BusinessException("Cliente com código " + codigoCliente + " não encontrado");
         }
         return cliente;
     }
 
-    private void validarClienteDto(ClienteDtoRequest dto) throws BusinessException {
-        if (dto.nome().isBlank()) {
-            throw new BusinessException("Nome não pode ser vazio.");
+    private ClienteEntity findByCpf(String cpf) throws BusinessException {
+        ClienteEntity cliente = clienteRepository.findByCpf(cpf);
+        if (cliente == null) {
+            throw new BusinessException("Cliente com CPF " + cpf + " não encontrado");
         }
-        if (dto.cpf().isBlank() || !dto.cpf().matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}")) {
-            throw new BusinessException("CPF inválido ou vazio. Deve estar no formato XXX.XXX.XXX-XX.");
-        }
-        if (clienteRepository.existsByCpf(dto.cpf())) {
+        return cliente;
+    }
+
+    private void verificaDuplicidade(ClienteDtoRequest dto, ClienteEntity clienteExistente) throws BusinessException {
+        if ((clienteExistente == null || !clienteExistente.getCpf().equals(dto.cpf())) && clienteRepository.existsByCpf(dto.cpf()))
             throw new BusinessException("CPF já cadastrado.");
-        }
-        if (dto.email().isBlank() || !dto.email().matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")) {
-            throw new BusinessException("Email inválido ou vazio. Formato esperado: exemplo@dominio.com");
-        }
-        if (clienteRepository.existsByEmail(dto.email())) {
+        if ((clienteExistente == null || !clienteExistente.getEmail().equals(dto.email())) && clienteRepository.existsByEmail(dto.email()))
             throw new BusinessException("Email já cadastrado.");
-        }
-        if (dto.cep().isBlank() || !dto.cep().matches("\\d{5}-\\d{3}")) {
+    }
+
+    private void validarEndereco(ClienteDtoRequest dto) throws BusinessException {
+        if (dto.cep().isBlank() || !dto.cep().matches("\\d{5}-\\d{3}"))
             throw new BusinessException("CEP inválido ou vazio. Deve estar no formato XXXXX-XXX.");
-        }
-        if (dto.logradouro().isBlank()) {
-            throw new BusinessException("Logradouro não pode ser vazio.");
-        }
-        if (dto.numero().isBlank()) {
-            throw new BusinessException("Número não pode ser vazio.");
-        }
-        if (dto.bairro().isBlank()) {
-            throw new BusinessException("Bairro não pode ser vazio.");
-        }
-        if (dto.cidade().isBlank()) {
-            throw new BusinessException("Cidade não pode ser vazia.");
-        }
-        if (dto.estado().isBlank()) {
-            throw new BusinessException("Estado não pode ser vazio.");
-        }
-        if (dto.telefone().isBlank() || !dto.telefone().matches("\\(\\d{2}\\) 9?\\d{4}-\\d{4}")) {
+        if (dto.logradouro().isBlank()) throw new BusinessException("Logradouro não pode ser vazio.");
+        if (dto.numero().isBlank()) throw new BusinessException("Número não pode ser vazio.");
+        if (dto.bairro().isBlank()) throw new BusinessException("Bairro não pode ser vazio.");
+        if (dto.cidade().isBlank()) throw new BusinessException("Cidade não pode ser vazia.");
+        if (dto.estado().isBlank()) throw new BusinessException("Estado não pode ser vazio.");
+        if (dto.telefone().isBlank() || !dto.telefone().matches("\\(\\d{2}\\) 9?\\d{4}-\\d{4}"))
             throw new BusinessException("Telefone inválido ou vazio. Deve estar no formato (XX) 9XXXX-XXXX.");
-        }
+    }
+
+    private void validarClienteDto(ClienteDtoRequest dto, ClienteEntity clienteExistente) throws BusinessException {
+        if (dto.nome().isBlank()) throw new BusinessException("Nome não pode ser vazio.");
+        if (dto.cpf().isBlank() || !dto.cpf().matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}"))
+            throw new BusinessException("CPF inválido ou vazio. Deve estar no formato XXX.XXX.XXX-XX.");
+        if (dto.email().isBlank() || !dto.email().matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"))
+            throw new BusinessException("Email inválido ou vazio. Formato esperado: exemplo@dominio.com");
+
+        verificaDuplicidade(dto, clienteExistente);
+
+        validarEndereco(dto);
     }
 
     public List<ClienteDtoResponse> listarClientes() {
@@ -68,47 +68,59 @@ public class ClienteService {
     }
 
     public ClienteDtoResponse buscarClientePorCodigo(Long codigoCliente) throws BusinessException {
-        ClienteEntity cliente = buscarClienteEntity(codigoCliente);
+        ClienteEntity cliente = findByCodigoCliente(codigoCliente);
+        return cliente.toDto();
+    }
+
+    public ClienteDtoResponse buscarClientePorCpf(String cpf) throws BusinessException {
+        ClienteEntity cliente = findByCpf(cpf);
         return cliente.toDto();
     }
 
     public ClienteDtoResponse cadastrarCliente(ClienteDtoRequest cliente) throws BusinessException {
-        validarClienteDto(cliente);
+        validarClienteDto(cliente, null);
         ClienteEntity novoCliente = cliente.toEntity();
         ClienteEntity clienteSalvo = clienteRepository.save(novoCliente);
         return clienteSalvo.toDto();
     }
 
-    public ClienteDtoResponse atualizarCliente(Long codigoCliente, ClienteDtoRequest clienteDto) throws BusinessException {
-        validarClienteDto(clienteDto);
-        ClienteEntity clienteExistente = buscarClienteEntity(codigoCliente);
-        if (!clienteExistente.getCpf().equals(clienteDto.cpf()) && clienteRepository.existsByCpf(clienteDto.cpf())) {
-            throw new BusinessException("CPF já cadastrado em outro registro.");
-        }
-        if (!clienteExistente.getEmail().equals(clienteDto.email()) && clienteRepository.existsByEmail(clienteDto.email())) {
-            throw new BusinessException("Email já cadastrado em outro registro.");
-        }
+    public ClienteDtoResponse atualizarCliente(ClienteDtoRequest clienteDto, ClienteEntity clienteExistente) throws BusinessException {
+        validarClienteDto(clienteDto, clienteExistente);
 
-        ClienteEntity clienteAtualizado = new ClienteEntity(
-            clienteExistente.getCodigoCliente(),
-            clienteDto.nome(),
-            clienteDto.cpf(),
-            clienteDto.email(),
-            clienteDto.cep(),
-            clienteDto.logradouro(),
-            clienteDto.numero(),
-            clienteDto.complemento(),
-            clienteDto.bairro(),
-            clienteDto.cidade(),
-            clienteDto.estado(),
-            clienteDto.telefone()
-        );
-        return clienteRepository.save(clienteAtualizado).toDto();
+        // Atualizar diretamente o clienteExistente em vez de criar um novo.
+        clienteExistente.setNome(clienteDto.nome());
+        clienteExistente.setCpf(clienteDto.cpf());
+        clienteExistente.setEmail(clienteDto.email());
+        clienteExistente.setCep(clienteDto.cep());
+        clienteExistente.setLogradouro(clienteDto.logradouro());
+        clienteExistente.setNumero(clienteDto.numero());
+        clienteExistente.setComplemento(clienteDto.complemento());
+        clienteExistente.setBairro(clienteDto.bairro());
+        clienteExistente.setCidade(clienteDto.cidade());
+        clienteExistente.setEstado(clienteDto.estado());
+        clienteExistente.setTelefone(clienteDto.telefone());
+
+        clienteRepository.save(clienteExistente);
+        return clienteExistente.toDto();
     }
 
-    public void excluirCliente(Long codigoCliente) throws BusinessException {
-        buscarClienteEntity(codigoCliente);
+    public ClienteDtoResponse atualizarClientePorCodigo(Long codigoCliente, ClienteDtoRequest clienteDto) throws BusinessException {
+        ClienteEntity clienteExistente = findByCodigoCliente(codigoCliente);
+        return atualizarCliente(clienteDto, clienteExistente);
+    }
+
+    public ClienteDtoResponse atualizarClientePorCpf(String cpf, ClienteDtoRequest clienteDto) throws BusinessException {
+        ClienteEntity clienteExistente = findByCpf(cpf);
+        return atualizarCliente(clienteDto, clienteExistente);
+    }
+
+    public void excluirClientePorCodigo(Long codigoCliente) throws BusinessException {
+        buscarClientePorCodigo(codigoCliente);
         clienteRepository.deleteById(codigoCliente);
     }
 
+    public void excluirClientePorCpf(String cpf) throws BusinessException {
+        ClienteEntity cliente = findByCpf(cpf);
+        clienteRepository.delete(cliente);
+    }
 }
